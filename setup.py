0f62b46fdb44e68779d87ee8e561d0261fcd6bab
Bartek Rutkowski <contact+git@robakdesign.com>
2014-07-24 13:27:57 +0100

    * setup.py - corrected egniter version, added requirements version numbers, added author data

--- setup.py
+++ setup.py
@@ -4,17 +4,16 @@ from os.path import join
 from setuptools import setup, find_packages
 
 setup(name='egniter',
-      version='1.0',
+      version='0.1',
       description='Egniter is a command line tool for easy launching VMWare' +
                   'ESX virtual machines using ESX API',
-      author='',
-      author_email='',
+      author='Bartek Rutkowski',
+      author_email='contact+egniter@robakdesign.com',
       license='BSD3',
       url='https://github.com/bartekrutkowski/egniter',
       packages=find_packages(),
       entry_points = {
           'console_scripts': ['egniter = egniter:main',]
       },
-      install_requires=['configparser', 'pysphere'],
+      install_requires=['configparser>=3.3.0', 'pysphere>=0.1.8'],
 )
-
